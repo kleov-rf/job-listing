3aa5a1ca593ecc452aa2cba9465f52fdea1f05c8
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-03 22:12:37 +0200

    test: show job details

---
+++ tests/unit/sections/jobs/components/JobCard.spec.tsx
@@ -0,0 +1,31 @@
+import {JobCard} from "@/sections/jobs/components/JobCard.tsx";
+import {render, screen} from "@testing-library/react";
+import {JobTypeEnum} from "@/modules/jobs/domain/value-objects";
+import {Job} from "@/modules/jobs/domain/entities/Job.ts";
+import {describe, it, expect} from "vitest";
+
+describe('JobCard', () => {
+    it('should show job details', () => {
+        const mockJob = Job.create({
+            id: '1',
+            title: 'Software Engineer',
+            description: 'Develop software applications',
+            location: 'Remote',
+            type: JobTypeEnum.FULL_TIME,
+            companyName: 'Tech Company',
+        })
+
+        render(<JobCard job={mockJob}/>)
+
+        expect(screen.getByTestId('job-title')).toBeInTheDocument()
+        expect(screen.getByText('Software Engineer')).toBeInTheDocument()
+        expect(screen.getByTestId('job-company-name')).toBeInTheDocument()
+        expect(screen.getByText('Tech Company')).toBeInTheDocument()
+        expect(screen.getByTestId('job-location')).toBeInTheDocument()
+        expect(screen.getByText('Remote')).toBeInTheDocument()
+        expect(screen.getByTestId('job-type')).toBeInTheDocument()
+        expect(screen.getByText('FULL TIME')).toBeInTheDocument()
+        expect(screen.getByTestId('job-description')).toBeInTheDocument()
+        expect(screen.getByText('Develop software applications')).toBeInTheDocument()
+    })
+})
