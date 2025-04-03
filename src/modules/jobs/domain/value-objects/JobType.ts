d8fa267f5017877764484cbfa595283ca4ef0526
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-03 20:17:49 +0200

    test: map api non defined employment status to domain job

--- src/modules/jobs/domain/value-objects/JobType.ts
+++ src/modules/jobs/domain/value-objects/JobType.ts
@@ -4,6 +4,7 @@ export enum JobTypeEnum {
     FULL_TIME = 'FULL_TIME',
     PART_TIME = 'PART_TIME',
     CONTRACT = 'CONTRACT',
+    NOT_DEFINED = 'NOT_DEFINED',
 }
 
 export class JobType extends ValueObject<JobTypeEnum> {
--- tests/unit/modules/jobs/infrastructure/JobEntityMapper.spec.ts
+++ tests/unit/modules/jobs/infrastructure/JobEntityMapper.spec.ts
@@ -85,5 +85,31 @@ describe('JobEntityMapper', () => {
             })
             expect(result).toEqual(expectedDomainJob)
         })
+        it('should map api non defined employment status job to domain job', () => {
+            const jobTitle = "Senior Accountant";
+            const companyName = 'Tech Corp';
+            const location = "Atlanta, GA";
+            const description = "We are looking for a Senior Accountant to join our team.";
+            const apiJob = {
+                id: 1,
+                job_title: jobTitle,
+                company: companyName,
+                short_location: location,
+                employment_statuses: [],
+                long_description: description,
+            } as APIJobDTO
+
+            const result = JobEntityMapper.toDomain(apiJob)
+
+            const expectedDomainJob = Job.create({
+                id: '1',
+                title: jobTitle,
+                companyName: companyName,
+                location: location,
+                type: JobTypeEnum.NOT_DEFINED,
+                description: description,
+            })
+            expect(result).toEqual(expectedDomainJob)
+        })
     })
 })
