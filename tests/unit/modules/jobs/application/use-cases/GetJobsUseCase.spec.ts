0564d2994aaf2a21ebd63b342c452f3320f3ba34
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-03 20:37:54 +0200

    test: return jobs from repository

--- tests/unit/modules/jobs/application/use-cases/GetJobsUseCase.spec.ts
+++ tests/unit/modules/jobs/application/use-cases/GetJobsUseCase.spec.ts
@@ -1,6 +1,8 @@
 import {describe, expect, it, vi} from "vitest";
 import {GetJobsUseCase} from "@/modules/jobs/application/use-cases/GetJobsUseCase";
 import {JobRepository} from "@/modules/jobs/domain/repositories/JobRepository";
+import {Job} from "@/modules/jobs/domain/entities/Job.ts";
+import {JobTypeEnum} from "@/modules/jobs/domain/value-objects";
 
 describe('GetJobsUseCase', () => {
     it('should call repository to find all jobs', async () => {
@@ -12,5 +14,25 @@ describe('GetJobsUseCase', () => {
         await getJobsUseCase.execute()
 
         expect(mockJobRepository.findAll).toHaveBeenCalled()
+    })
+    it('should return jobs from repository', async () => {
+        const mockJobs = [
+            Job.create({
+                id: '1',
+                title: 'Software Engineer',
+                description: 'Develop software applications',
+                location: 'Remote',
+                type: JobTypeEnum.FULL_TIME,
+                companyName: 'Tech Company',
+            })
+        ]
+        const mockJobRepository = {
+            findAll: vi.fn().mockResolvedValue(mockJobs),
+        } as unknown as JobRepository
+        const getJobsUseCase = new GetJobsUseCase(mockJobRepository)
+
+        const result = await getJobsUseCase.execute()
+
+        expect(result).toEqual(mockJobs)
     })
 })
