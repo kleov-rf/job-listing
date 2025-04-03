5511b10172b856b14463e77558c92e8156d9e43d
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-03 19:26:42 +0200

    feat: call to map API response to domain job

--- src/modules/jobs/infrastructure/repositories/APIJobRepository.ts
+++ src/modules/jobs/infrastructure/repositories/APIJobRepository.ts
@@ -1,13 +1,16 @@
 import {JobRepository} from "@/modules/jobs/domain/repositories/JobRepository.ts";
 import {Job} from "@/modules/jobs/domain/entities/Job.ts";
+import {APIJobsResponseDTO} from "@/modules/jobs/infrastructure/dtos/APIJobResponseDTO.ts";
+import {JobEntityMapper} from "@/modules/jobs/infrastructure/mappers/JobEntityMapper.ts";
 
 export class APIJobRepository implements JobRepository {
     private readonly apiUrl = 'https://api.theirstack.com/v1/jobs/search'
     private readonly apiKey = import.meta.env.VITE_API_KEY
 
     async findAll(): Promise<Job[]> {
-        await this.fetchJobs();
+        const {data} = await this.fetchJobs() as APIJobsResponseDTO;
 
+        JobEntityMapper.toDomain(data[0])
         return []
     }
 
@@ -19,13 +22,15 @@ export class APIJobRepository implements JobRepository {
             job_country_code_or: ['US'],
             posted_at_max_age_days: 7,
         };
-        await fetch(this.apiUrl, {
+        const response = await fetch(this.apiUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 Authorization: `Bearer ${this.apiKey}`,
             },
             body: JSON.stringify(defaultOptions),
-        })
+        });
+
+        return response.json()
     }
 }
