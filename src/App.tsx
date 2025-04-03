e00b16c025df5e5cb6766c2af0fed3dddfa92feb
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-03 21:09:28 +0200

    feat: use job provider context

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { JobsDashboard } from "@/sections/jobs/JobsDashboard"
+import {JobsDashboard} from "@/sections/jobs/JobsDashboard"
+import {JobProvider} from "@/sections/context/JobContext.tsx";
 
 function App() {
     return (
-        <div className="container mx-auto px-4 py-8">
-            <header className="mb-8 flex justify-between items-center">
-                <h1 className="text-3xl font-bold">Job Listings</h1>
-            </header>
-            <main>
-                <JobsDashboard />
-            </main>
-        </div>
+        <JobProvider>
+            <div className="container mx-auto px-4 py-8">
+                <header className="mb-8 flex justify-between items-center">
+                    <h1 className="text-3xl font-bold">Job Listings</h1>
+                </header>
+                <main>
+                    <JobsDashboard/>
+                </main>
+            </div>
+        </JobProvider>
     )
 }
 
--- src/sections/context/JobContext.tsx
+++ src/sections/context/JobContext.tsx
@@ -1,11 +1,23 @@
 import {createContext, useContext} from "react";
 import {GetJobsUseCase} from "@/modules/jobs/application/use-cases/GetJobsUseCase.ts";
+import {APIJobRepository} from "@/modules/jobs/infrastructure/repositories/APIJobRepository.ts";
 
 export interface JobContextType {
     getJobsUseCase: GetJobsUseCase;
 }
 
 export const JobContext = createContext<JobContextType | undefined>(undefined)
+
+export const JobProvider = ({children}: { children: React.ReactNode }) => {
+    const jobRepository = new APIJobRepository();
+    const getJobsUseCase = new GetJobsUseCase(jobRepository);
+
+    const value = {
+        getJobsUseCase,
+    }
+
+    return <JobContext.Provider value={value}>{children}</JobContext.Provider>
+}
 
 export const useJobContext = (): JobContextType => {
     const context = useContext(JobContext)
