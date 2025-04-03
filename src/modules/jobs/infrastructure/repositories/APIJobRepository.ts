79dccbe46b022127dbd4d7eedd9c263399a2f125
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-03 18:43:51 +0200

    feat: call API with default options to find all jobs

--- src/modules/jobs/infrastructure/repositories/APIJobRepository.ts
+++ src/modules/jobs/infrastructure/repositories/APIJobRepository.ts
@@ -3,6 +3,21 @@ import {Job} from "@/modules/jobs/domain/entities/Job.ts";
 
 export class APIJobRepository implements JobRepository {
     async findAll(): Promise<Job[]> {
-        throw new Error("Method not implemented.");
+        await fetch('https://api.theirstack.com/v1/jobs/search', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+            },
+            body: JSON.stringify({
+                page: 0,
+                limit: 25,
+                blur_company_data: true,
+                job_country_code_or: ['US'],
+                posted_at_max_age_days: 7,
+            }),
+        })
+
+        return []
     }
 }
