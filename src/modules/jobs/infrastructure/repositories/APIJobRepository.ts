c0fc97a2622d9ee4e10d4f2157941d7eed32d355
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-03 19:03:38 +0200

    refactor: extract common behavior

--- src/modules/jobs/infrastructure/repositories/APIJobRepository.ts
+++ src/modules/jobs/infrastructure/repositories/APIJobRepository.ts
@@ -2,22 +2,30 @@ import {JobRepository} from "@/modules/jobs/domain/repositories/JobRepository.ts
 import {Job} from "@/modules/jobs/domain/entities/Job.ts";
 
 export class APIJobRepository implements JobRepository {
+    private readonly apiUrl = 'https://api.theirstack.com/v1/jobs/search'
+    private readonly apiKey = import.meta.env.VITE_API_KEY
+
     async findAll(): Promise<Job[]> {
-        await fetch('https://api.theirstack.com/v1/jobs/search', {
+        await this.fetchJobs();
+
+        return []
+    }
+
+    private async fetchJobs() {
+        const defaultOptions = {
+            page: 0,
+            limit: 25,
+            blur_company_data: true,
+            job_country_code_or: ['US'],
+            posted_at_max_age_days: 7,
+        };
+        await fetch(this.apiUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+                Authorization: `Bearer ${this.apiKey}`,
             },
-            body: JSON.stringify({
-                page: 0,
-                limit: 25,
-                blur_company_data: true,
-                job_country_code_or: ['US'],
-                posted_at_max_age_days: 7,
-            }),
+            body: JSON.stringify(defaultOptions),
         })
-
-        return []
     }
 }
