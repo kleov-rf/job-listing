3e3f81db132949cdcd46d7f58c68237fdc380d18
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-03 21:19:46 +0200

    test: show jobs

--- tests/unit/sections/jobs/JobsDashboard.spec.tsx
+++ tests/unit/sections/jobs/JobsDashboard.spec.tsx
@@ -1,7 +1,9 @@
-import {render} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 import {describe, expect, it, vi} from "vitest";
 import {JobContextType, JobContext} from "@/sections/context/JobContext";
 import {JobsDashboard} from "@/sections/jobs/JobsDashboard";
+import {Job} from "@/modules/jobs/domain/entities/Job.ts";
+import {JobTypeEnum} from "@/modules/jobs/domain/value-objects";
 
 describe('JobsDashboard', () => {
     it('should call to get jobs', () => {
@@ -19,5 +21,40 @@ describe('JobsDashboard', () => {
         )
 
         expect(mockGetJobsUseCase.execute).toHaveBeenCalled()
+    })
+    it('should show jobs', async () => {
+        const mockRetrievedJobs = [
+            Job.create({
+                id: '1',
+                title: 'Software Engineer',
+                description: 'Develop software applications',
+                location: 'Remote',
+                type: JobTypeEnum.FULL_TIME,
+                companyName: 'Tech Company',
+            }),
+            Job.create({
+                id: '2',
+                title: 'Data Scientist',
+                description: 'Analyze data and build models',
+                location: 'Remote',
+                type: JobTypeEnum.PART_TIME,
+                companyName: 'Data Corp',
+            }),
+        ];
+        const mockGetJobsUseCase = {
+            execute: vi.fn().mockResolvedValue(mockRetrievedJobs)
+        }
+        const mockJobContext = {
+            getJobsUseCase: mockGetJobsUseCase,
+        } as unknown as JobContextType
+
+        render(
+            <JobContext.Provider value={mockJobContext}>
+                <JobsDashboard />
+            </JobContext.Provider>
+        )
+
+        const jobCards = await screen.findAllByTestId('job-card');
+        expect(jobCards).toHaveLength(mockRetrievedJobs.length);
     })
 })
