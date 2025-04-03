e6661e705f2b5e0dde5dad9653161d68e55e2514
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-03 21:19:55 +0200

    feat: show jobs

--- src/sections/jobs/JobsDashboard.tsx
+++ src/sections/jobs/JobsDashboard.tsx
@@ -1,16 +1,24 @@
-import {JSX, useEffect} from "react";
+import {JSX, useEffect, useMemo, useState} from "react";
 import {useJobContext} from "@/sections/context/JobContext.tsx";
+import {Job} from "@/modules/jobs/domain/entities/Job.ts";
 
 export const JobsDashboard: () => JSX.Element = () => {
-    const { getJobsUseCase } = useJobContext();
+    const [jobs, setJobs] = useState<Job[]>([]);
+    const {getJobsUseCase} = useJobContext();
 
     useEffect(() => {
         const fetchJobs = async () => {
-            await getJobsUseCase.execute();
+            setJobs(await getJobsUseCase.execute());
         };
 
         fetchJobs();
     })
 
-    return <></>;
+    const jobCards = useMemo(() => {
+        return jobs.map(() => (
+            <div data-testid="job-card"></div>
+        ));
+    }, [jobs]);
+
+    return <>{ jobCards }</>;
 }
