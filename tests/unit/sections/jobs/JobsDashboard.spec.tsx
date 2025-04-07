68511ae651f9efaf65198352f4dcfdd8e28afae4
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-07 19:07:40 +0200

    test: show empty message when no job is retrieved

--- tests/unit/sections/jobs/JobsDashboard.spec.tsx
+++ tests/unit/sections/jobs/JobsDashboard.spec.tsx
@@ -1,4 +1,4 @@
-import {render, waitFor} from "@testing-library/react";
+import {render, waitFor, screen} from "@testing-library/react";
 import {describe, expect, it, vi} from "vitest";
 import {JobContextType, JobContext} from "@/sections/context/JobContext";
 import {JobsDashboard} from "@/sections/jobs/JobsDashboard";
@@ -39,5 +39,22 @@ describe('JobsDashboard', () => {
         await waitFor(() => {
             expect(mockGetJobsUseCase.execute).not.toHaveBeenCalledTimes(2)
         })
+    })
+    it('should show empty message when no job is retrieved', async () => {
+        const mockGetJobsUseCase = {
+            execute: vi.fn().mockResolvedValue([])
+        }
+        const mockJobContext = {
+            getJobsUseCase: mockGetJobsUseCase,
+        } as unknown as JobContextType
+
+        render(
+            <JobContext.Provider value={mockJobContext}>
+                <JobsDashboard/>
+            </JobContext.Provider>
+        )
+
+        const emptyMessage = screen.getByText('No jobs found.')
+        expect(emptyMessage).toBeInTheDocument()
     })
 })
