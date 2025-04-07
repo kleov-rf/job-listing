ba3ab14aaf16e40f7b587681c7359dcd1ac03ab8
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-08 00:07:48 +0200

    feat: return - for NOT_DEFINED

--- src/sections/jobs/utils/renderJobTypeLabel.ts
+++ src/sections/jobs/utils/renderJobTypeLabel.ts
@@ -1,6 +1,9 @@
 import {JobTypeEnum} from "@/modules/jobs/domain/value-objects";
 
 export const renderJobTypeLabel = (jobType: JobTypeEnum): string => {
+    if (jobType === JobTypeEnum.NOT_DEFINED) {
+        return '-';
+    }
     if (jobType === JobTypeEnum.CONTRACT) {
         return 'Contract';
     }
