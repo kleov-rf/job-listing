a590933180b51ea79629fa99f284a9cc539c39ad
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-07 18:32:29 +0200

    test: call to get jobs only once

--- tests/unit/sections/jobs/JobsDashboard.spec.tsx
+++ tests/unit/sections/jobs/JobsDashboard.spec.tsx
@@ -1,4 +1,4 @@
-import {render, screen} from "@testing-library/react";
+import {render, screen, waitFor} from "@testing-library/react";
 import {describe, expect, it, vi} from "vitest";
 import {JobContextType, JobContext} from "@/sections/context/JobContext";
 import {JobsDashboard} from "@/sections/jobs/JobsDashboard";
@@ -16,11 +16,31 @@ describe('JobsDashboard', () => {
 
         render(
             <JobContext.Provider value={mockJobContext}>
-                <JobsDashboard />
+                <JobsDashboard/>
             </JobContext.Provider>
         )
 
         expect(mockGetJobsUseCase.execute).toHaveBeenCalled()
+    })
+    it('should call to get jobs only once', async () => {
+        const mockGetJobsUseCase = {
+            execute: vi.fn().mockResolvedValue([])
+        }
+        const mockJobContext = {
+            getJobsUseCase: mockGetJobsUseCase,
+        } as unknown as JobContextType
+
+        render(
+            <JobContext.Provider value={mockJobContext}>
+                <JobsDashboard/>
+            </JobContext.Provider>
+        )
+
+        expect(mockGetJobsUseCase.execute).toHaveBeenCalledTimes(1)
+
+        await waitFor(() => {
+            expect(mockGetJobsUseCase.execute).not.toHaveBeenCalledTimes(2)
+        })
     })
     it('should show jobs', async () => {
         const mockRetrievedJobs = [
@@ -50,7 +70,7 @@ describe('JobsDashboard', () => {
 
         render(
             <JobContext.Provider value={mockJobContext}>
-                <JobsDashboard />
+                <JobsDashboard/>
             </JobContext.Provider>
         )
 
