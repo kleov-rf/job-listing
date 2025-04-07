d8933408d75e9a81fbdbec9d8e7768d5008e0f8c
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-08 00:05:08 +0200

    feat: return Part-Time for PART_TIME

--- src/sections/jobs/utils/renderJobTypeLabel.ts
+++ src/sections/jobs/utils/renderJobTypeLabel.ts
@@ -1,5 +1,8 @@
 import {JobTypeEnum} from "@/modules/jobs/domain/value-objects";
 
 export const renderJobTypeLabel = (jobType: JobTypeEnum): string => {
+    if (jobType === JobTypeEnum.PART_TIME) {
+        return 'Part-Time';
+    }
     return 'Full-Time';
 }
