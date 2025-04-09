5728ca8394052c050f57551711728e414159e46d
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-09 17:44:34 +0200

    test: call localStorage when saving to get existing job applications

--- tests/unit/modules/job-applications/infrastructure/repositories/LocalStorageJobApplicationRepository.spec.ts
+++ tests/unit/modules/job-applications/infrastructure/repositories/LocalStorageJobApplicationRepository.spec.ts
@@ -23,4 +23,19 @@ describe('LocalStorageJobApplicationRepository', () => {
         )
         expect(mockLocalStorage.setItem).toHaveBeenCalledWith('job_applications', expectedJobApplication)
     })
+    it('should call localStorage when saving to get existing job applications', () => {
+        const mockLocalStorage = {
+            setItem: vi.fn(),
+            getItem: vi.fn().mockReturnValue(null)
+        } as unknown as Storage
+        Object.defineProperty(window, "localStorage", {
+            value: mockLocalStorage
+        })
+        const mockJobApplication = JobApplicationMother.createDefault()
+        const repository = new LocalStorageJobApplicationRepository()
+
+        repository.save(mockJobApplication)
+
+        expect(mockLocalStorage.getItem).toHaveBeenCalledWith('job_applications')
+    })
 })
