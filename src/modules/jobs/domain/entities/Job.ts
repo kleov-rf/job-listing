a14bbc1ce8c5d872f5f23afcd7b210cd5ac07df1
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-09 08:36:41 +0200

    feat: match job by lowercase company name

--- src/modules/jobs/domain/entities/Job.ts
+++ src/modules/jobs/domain/entities/Job.ts
@@ -61,7 +61,7 @@ export class Job {
 
     matchesSearch(jobTitle: string) {
         const lowerCaseJobTitle = jobTitle.toLowerCase();
-        if(this.companyNameValue().includes(jobTitle)) {
+        if(this.companyNameValue().toLowerCase().includes(jobTitle)) {
             return true;
         }
         return this.titleValue().toLowerCase().includes(lowerCaseJobTitle)
