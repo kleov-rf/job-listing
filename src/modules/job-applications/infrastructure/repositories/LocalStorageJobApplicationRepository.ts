13d5f57410dea4f5836f5d38a65fbb67133630eb
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-09 17:45:27 +0200

    feat: call localStorage when saving to get existing job applications

--- src/modules/job-applications/infrastructure/repositories/LocalStorageJobApplicationRepository.ts
+++ src/modules/job-applications/infrastructure/repositories/LocalStorageJobApplicationRepository.ts
@@ -1,9 +1,10 @@
 import {JobApplicationRepository} from "@/modules/job-applications/domain/repositories/JobApplicationRepository.ts";
 import {JobApplication} from "@/modules/jobs/domain/entities/JobApplication.ts";
+import {Primitives} from "@codelytv/primitives-type";
 
 export class LocalStorageJobApplicationRepository implements JobApplicationRepository {
     save(jobApplication: JobApplication): Promise<void> {
-        const jobApplications = new Map();
+        const jobApplications = this.getAllFromLocalStorage();
         const jobApplicationPrimitives = jobApplication.toPrimitives();
 
         jobApplications.set(jobApplicationPrimitives.jobId, jobApplicationPrimitives);
@@ -11,5 +12,15 @@ export class LocalStorageJobApplicationRepository implements JobApplicationRepos
         localStorage.setItem("job_applications", JSON.stringify(Array.from(jobApplications.entries())));
 
         return Promise.resolve();
+    }
+
+    private getAllFromLocalStorage(): Map<string, Primitives<JobApplication>> {
+        const jobApplications = localStorage.getItem("job_applications");
+
+        if (jobApplications === null) {
+            return new Map();
+        }
+
+        return new Map(JSON.parse(jobApplications) as Iterable<[string, Primitives<JobApplication>]>);
     }
 }
