1f96d78dd3e58298a16ad25d5db79729ddce9a4d
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-09 19:48:51 +0200

    style: already applied badge

--- src/sections/jobs/components/JobDetailsCard.tsx
+++ src/sections/jobs/components/JobDetailsCard.tsx
@@ -15,7 +15,7 @@ export const JobDetailsCard = ({job, onApply, hasApplied}: JobDetailsCardProps) 
             <CardHeader>
                 {hasApplied && (
                     <span
-                        className="bg-green-100 text-green-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-green-900 dark:text-green-300">
+                        className="w-fit bg-green-100 text-green-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-green-900 dark:text-green-300">
                         Already applied
                     </span>
                 )}
