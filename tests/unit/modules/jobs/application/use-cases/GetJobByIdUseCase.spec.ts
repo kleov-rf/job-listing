42e3e6b1f7afab4c063c819a2cba8a9a81ba36f6
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-09 18:02:04 +0200

    fix: wrap with job id call to repository to find job

--- tests/unit/modules/jobs/application/use-cases/GetJobByIdUseCase.spec.ts
+++ tests/unit/modules/jobs/application/use-cases/GetJobByIdUseCase.spec.ts
@@ -2,6 +2,7 @@ import {describe, expect, it, vi} from "vitest";
 import {JobRepository} from "@/modules/jobs/domain/repositories/JobRepository.ts";
 import {GetJobByIdUseCase} from "@/modules/jobs/application/use-cases/GetJobByIdUseCase.ts";
 import {JobMother} from "../../domain/entities/JobMother.ts";
+import {JobId} from "@/modules/jobs/domain/value-objects";
 
 describe('GetJobByIdUseCase', () => {
     it('should call repository to find job by id', async () => {
@@ -13,7 +14,7 @@ describe('GetJobByIdUseCase', () => {
 
         await getJobByIdUseCase.execute(jobId);
 
-        expect(mockJobRepository.findById).toHaveBeenCalledWith(jobId);
+        expect(mockJobRepository.findById).toHaveBeenCalledWith(new JobId(jobId));
     })
     it('should return job if found', async () => {
         const mockJobId = '12345';
