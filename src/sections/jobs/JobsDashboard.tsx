5af7e980a51eaad8185ab6cb740a9e0e5793ac95
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-09 13:47:57 +0200

    feat: show skeleton loader when jobs are being fetched

--- src/sections/jobs/JobsDashboard.tsx
+++ src/sections/jobs/JobsDashboard.tsx
@@ -3,15 +3,18 @@ import {useJobContext} from "@/sections/context/JobContext.tsx";
 import {Job} from "@/modules/jobs/domain/entities/Job.ts";
 import {JobList} from "@/sections/jobs/components/JobList.tsx";
 import {useNavigate} from "react-router-dom";
+import {Card} from "@/sections/shared/components/Card.tsx";
 
 export const JobsDashboard: () => JSX.Element = () => {
     const [jobs, setJobs] = useState<Job[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
     const {getJobsUseCase} = useJobContext();
     const navigate = useNavigate();
 
     useEffect(() => {
         const fetchJobs = async () => {
             setJobs(await getJobsUseCase.execute());
+            setIsLoading(false);
         };
 
         fetchJobs();
@@ -19,6 +22,20 @@ export const JobsDashboard: () => JSX.Element = () => {
 
     const handleJobApply = (jobId: string) => {
         navigate(`/jobs/${jobId}`)
+    }
+
+    if (isLoading) {
+        return (
+            <section className="space-y-6" role="status">
+                <section className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-start animate-pulse">
+                    <article className="h-9 w-full bg-zinc-200 sm:w-44"></article>
+                    <article className="h-9 w-full bg-zinc-200 sm:w-44"></article>
+                </section>
+                <section className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 w-full animate-pulse">
+                    {Array.from({length: 9}).map((_, index) => <Card key={index} className="w-full h-80 bg-zinc-200"></Card>)}
+                </section>
+            </section>
+        );
     }
 
     return (
