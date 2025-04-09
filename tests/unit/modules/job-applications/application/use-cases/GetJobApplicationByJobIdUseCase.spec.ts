de18f4cdd0353d2f8e18ed7780ce412cbbdbcbd4
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-09 19:21:21 +0200

    test: return job application by id

--- tests/unit/modules/job-applications/application/use-cases/GetJobApplicationByJobIdUseCase.spec.ts
+++ tests/unit/modules/job-applications/application/use-cases/GetJobApplicationByJobIdUseCase.spec.ts
@@ -4,6 +4,7 @@ import {
 } from "@/modules/job-applications/application/use-cases/GetJobApplicationByJobIdUseCase.ts";
 import {JobApplicationRepository} from "@/modules/job-applications/domain/repositories/JobApplicationRepository.ts";
 import {JobId} from "@/modules/jobs/domain/value-objects";
+import {JobApplicationMother} from "../../domain/entities/JobApplicationMother.ts";
 
 describe('GetJobApplicationByJobIdUseCase', () => {
     it('should call to get job application by job id', async () => {
@@ -16,5 +17,16 @@ describe('GetJobApplicationByJobIdUseCase', () => {
         await useCase.execute(mockJobId);
 
         expect(jobApplicationRepository.getByJobId).toHaveBeenCalledWith(new JobId(mockJobId))
+    })
+    it('should return job application by job id', async () => {
+        const mockJobApplication = JobApplicationMother.createDefault()
+        const jobApplicationRepository = {
+            getByJobId: vi.fn().mockResolvedValue([mockJobApplication])
+        } as unknown as JobApplicationRepository;
+        const useCase = new GetJobApplicationByJobIdUseCase(jobApplicationRepository)
+
+        const retrievedJobApplication = await useCase.execute(mockJobApplication.jobIdValue());
+
+        expect(retrievedJobApplication).toEqual([mockJobApplication])
     })
 })
