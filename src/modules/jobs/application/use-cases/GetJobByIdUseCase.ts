d00cc1350456b18c258c0091f2be37fb55a0d982
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-09 12:51:10 +0200

    feat: return job if found

--- src/modules/jobs/application/use-cases/GetJobByIdUseCase.ts
+++ src/modules/jobs/application/use-cases/GetJobByIdUseCase.ts
@@ -4,8 +4,7 @@ import {Job} from "@/modules/jobs/domain/entities/Job.ts";
 export class GetJobByIdUseCase {
   constructor(private readonly jobRepository: JobRepository) {}
 
-    async execute(jobId: string): Promise<Job> {
-        await this.jobRepository.findById(jobId)
-        return null;
+    async execute(jobId: string): Promise<Job[]> {
+        return await this.jobRepository.findById(jobId)
     }
 }
