bdf2906b4a04fb6bf7d3cefabc472e15d5ae70dc
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-09 17:41:57 +0200

    test: call localStorage to save job application

---
+++ tests/unit/modules/job-applications/infrastructure/repositories/LocalStorageJobApplicationRepository.spec.ts
@@ -0,0 +1,26 @@
+import {describe, expect, it, vi} from "vitest"
+import {
+    LocalStorageJobApplicationRepository
+} from "@/modules/job-applications/infrastructure/repositories/LocalStorageJobApplicationRepository.ts";
+import {JobApplicationMother} from "../../../jobs/domain/entities/JobApplicationMother.ts";
+
+describe('LocalStorageJobApplicationRepository', () => {
+    it('should call localStorage to save job application', () => {
+        const mockLocalStorage = {
+            setItem: vi.fn(),
+            getItem: vi.fn().mockReturnValue(null)
+        } as unknown as Storage
+        Object.defineProperty(window, "localStorage", {
+            value: mockLocalStorage
+        })
+        const mockJobApplication = JobApplicationMother.createDefault()
+        const repository = new LocalStorageJobApplicationRepository()
+
+        repository.save(mockJobApplication)
+
+        const expectedJobApplication = JSON.stringify(
+            Array.from(new Map().set(mockJobApplication.jobIdValue(), mockJobApplication.toPrimitives()).entries())
+        )
+        expect(mockLocalStorage.setItem).toHaveBeenCalledWith('job_applications', expectedJobApplication)
+    })
+})
