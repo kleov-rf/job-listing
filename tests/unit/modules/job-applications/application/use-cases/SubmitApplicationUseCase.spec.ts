cbd71a814f552c0756edf6303c3fcddf732be23f
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-09 17:50:33 +0200

    test: call to verify if job exists

--- tests/unit/modules/job-applications/application/use-cases/SubmitApplicationUseCase.spec.ts
+++ tests/unit/modules/job-applications/application/use-cases/SubmitApplicationUseCase.spec.ts
@@ -4,6 +4,8 @@ import {JobApplicationRepository} from "@/modules/job-applications/domain/reposi
 import {JobRepository} from "@/modules/jobs/domain/repositories/JobRepository.ts";
 import {Primitives} from "@codelytv/primitives-type";
 import {JobApplication} from "@/modules/jobs/domain/entities/JobApplication.ts";
+import {v4} from "uuid";
+import {JobId} from "@/modules/jobs/domain/value-objects";
 
 describe('SubmitApplicationUseCase', () => {
     it('should call to save application', () => {
@@ -13,7 +15,7 @@ describe('SubmitApplicationUseCase', () => {
         const mockJobRepository = vi.fn() as unknown as JobRepository;
 
         const jobApplicationData = {
-            id: 'dc8b2f9e-8ce4-4d4c-97a5-165815bc3678',
+            id: v4().toString(),
             name: 'John Doe',
             email: 'john.doe@gmail.com',
             cvUrl: 'https://example.com/cv.pdf',
@@ -24,5 +26,27 @@ describe('SubmitApplicationUseCase', () => {
         submitApplicationUseCase.execute(jobApplicationData)
 
         expect(mockJobApplicationRepository.save).toHaveBeenCalledWith(JobApplication.create(jobApplicationData))
+    })
+    it('should call to verify if job exists', () => {
+        const mockJobApplicationRepository = {
+            save: vi.fn(),
+        } as unknown as JobApplicationRepository;
+        const mockJobRepository = {
+            findById: vi.fn()
+        } as unknown as JobRepository;
+
+        const jobApplicationData = {
+            id: v4().toString(),
+            name: 'John Doe',
+            email: 'john.doe@gmail.com',
+            cvUrl: 'https://example.com/cv.pdf',
+            jobId: '1234',
+        } as Primitives<JobApplication>;
+        const submitApplicationUseCase = new SubmitApplicationUseCase(mockJobApplicationRepository, mockJobRepository)
+
+        submitApplicationUseCase.execute(jobApplicationData)
+
+        const expectedJobId = new JobId(jobApplicationData.jobId);
+        expect(mockJobRepository.findById).toHaveBeenCalledWith(expectedJobId)
     })
 })
