ae30d24bbadb425d2c30d46f671a060d1e370030
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-09 19:02:11 +0200

    feat: call to get job application by job id

--- src/modules/job-applications/application/use-cases/GetJobApplicationByJobIdUseCase.ts
+++ src/modules/job-applications/application/use-cases/GetJobApplicationByJobIdUseCase.ts
@@ -1,10 +1,11 @@
 import {JobApplicationRepository} from "@/modules/job-applications/domain/repositories/JobApplicationRepository.ts";
 import {JobApplication} from "@/modules/job-applications/domain/entities/JobApplication.ts";
+import {JobId} from "@/modules/jobs/domain/value-objects";
 
 export class GetJobApplicationByJobIdUseCase {
     constructor(private readonly repository: JobApplicationRepository) {}
 
     async execute(jobId: string): Promise<JobApplication[]> {
-        throw new Error("Method not implemented.");
+        await this.repository.getByJobId(new JobId(jobId))
     }
 }
