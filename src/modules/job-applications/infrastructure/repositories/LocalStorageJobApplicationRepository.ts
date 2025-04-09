c4ff5a46b2601fb005d1f0332b9001ba67881508
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2025-04-09 17:42:03 +0200

    feat: call localStorage to save job application

--- src/modules/job-applications/infrastructure/repositories/LocalStorageJobApplicationRepository.ts
+++ src/modules/job-applications/infrastructure/repositories/LocalStorageJobApplicationRepository.ts
@@ -1,9 +1,15 @@
 import {JobApplicationRepository} from "@/modules/job-applications/domain/repositories/JobApplicationRepository.ts";
-import {SaveJobApplicationDTO} from "@/modules/job-applications/application/dtos.ts";
+import {JobApplication} from "@/modules/jobs/domain/entities/JobApplication.ts";
 
 export class LocalStorageJobApplicationRepository implements JobApplicationRepository {
-    save(jobApplication: SaveJobApplicationDTO): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
+    save(jobApplication: JobApplication): Promise<void> {
+        const jobApplications = new Map();
+        const jobApplicationPrimitives = jobApplication.toPrimitives();
 
+        jobApplications.set(jobApplicationPrimitives.jobId, jobApplicationPrimitives);
+
+        localStorage.setItem("job_applications", JSON.stringify(Array.from(jobApplications.entries())));
+
+        return Promise.resolve();
+    }
 }
